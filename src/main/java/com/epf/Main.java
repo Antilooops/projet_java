06797400236238c09dc9640e551bc1c2fc86a0e7
Antilooops <louissavignac333@gmail.com>
2025-03-10 15:58:45 +0100

    feat: rest of the project

---
+++ src/main/java/com/epf/Main.java
@@ -0,0 +1,9 @@
+package com.epf;
+
+import com.epf.config.DBConfig;
+
+public class Main {
+    public static void main(String[] args) {
+        DBConfig 
+    }
+}
--- src/main/java/com/epf/repository/PlantRepository.java
+++
@@ -1,1 +0,0 @@
-package com.epf.repository;
