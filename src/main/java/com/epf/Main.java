912da24c841861b0a28a808704987bdebc0f02c0
Antilooops <louissavignac333@gmail.com>
2025-03-17 16:20:50 +0100

    feat: Main class for very simple test

--- src/main/java/com/epf/Main.java
+++ src/main/java/com/epf/Main.java
@@ -1,9 +1,19 @@
 package com.epf;
 
-import com.epf.persistance.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.epf.config.JDBCconfig;
+import com.epf.persistance.dao.PlantsDAO;
+import com.epf.persistance.entity.PlantsEntity;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JDBConfig.class);
+        System.out.println("\n");
 
+        System.out.println("\n");
+        context.close();
     }
 }
