5bfd2c5e2bb15522ac49611470f043478134f3dd
Antilooops <louissavignac333@gmail.com>
2025-03-17 16:19:55 +0100

    feat: simple plants repository

--- src/main/java/com/epf/persistance/repository/PlantsRepository.java
+++ src/main/java/com/epf/persistance/repository/PlantsRepository.java
@@ -2,7 +2,9 @@ package com.epf.persistance.repository;
 
 import com.epf.core.model.Plants;
 import com.epf.persistance.dao.PlantsDAO;
+import com.epf.persistance.mapper.PlantsEntityMapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,12 +13,19 @@ import java.util.List;
 public class PlantsRepository {
 
     private final PlantsDAO plantsDAO;
+    private final PlantsEntityMapper plantsMapper;
 
-    public PlantsRepository(PlantsDAO plantsDAO) {
+    @Autowired
+    public PlantsRepository(PlantsDAO plantsDAO, PlantsEntityMapper plantsEntityMapper) {
         this.plantsDAO = plantsDAO;
+        this.plantsMapper = plantsEntityMapper;
     }
 
     public List<Plants> getAllPlants() {
-        return this.plantsDAO.getAllPlants();
+        return plantsMapper.mapListPlantsEntitiesToListPlantsModels(plantsDAO.getAllPlants());
+    }
+
+    public Plants getPlantByID(int id) {
+        return plantsMapper.mapPlantEntityToPlantModel(plantsDAO.getPlantById(id));
     }
 }
